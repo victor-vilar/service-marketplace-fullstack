24e00b11e1d881b82f8885fb6f84c34c6230928e
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 13:40:20 -0300

    fix:corrigido pacote do loginController.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
@@ -1,4 +1,4 @@
-package com.victorvilar.marketplace.fullstack;
+package com.victorvilar.marketplace.fullstack.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
