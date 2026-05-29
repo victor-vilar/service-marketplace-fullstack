56c98b9f1764176644578a2633da87c3495c0220
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 13:38:09 -0300

    criado o loginController.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/LoginController.java
@@ -0,0 +1,17 @@
+package com.victorvilar.marketplace.fullstack;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+    @GetMapping
+    public String login(){
+        return "logado";
+    }
+
+
+}
