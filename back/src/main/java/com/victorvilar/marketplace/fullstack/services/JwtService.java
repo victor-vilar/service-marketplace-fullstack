b5d4565fb67e5ba4973a49e58c6a53ac77720359
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 14:40:57 -0300

    fix:adicionado o jwtservice no pacote correto.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/services/JwtService.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/services/JwtService.java
@@ -1,10 +1,9 @@
-package com.victorvilar.marketplace.fullstack;
+package com.victorvilar.marketplace.fullstack.services;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
