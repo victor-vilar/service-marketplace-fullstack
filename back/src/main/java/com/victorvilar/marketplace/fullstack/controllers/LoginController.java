903cd5ffcb9307252e73f3f0118e1b45aca62558
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 16:13:08 -0300

    feat:adicionada dependencias no loginController.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
@@ -1,5 +1,8 @@
 package com.victorvilar.marketplace.fullstack.controllers;
 
+import com.victorvilar.marketplace.fullstack.services.JwtService;
+import com.victorvilar.marketplace.fullstack.services.implementation.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/login")
 public class LoginController {
+
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Autowired
+    public LoginController(JwtService jwtService, UserService userService){
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
 
     @GetMapping
     public String login(){
