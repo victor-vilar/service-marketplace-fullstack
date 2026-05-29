f186a6d548236f4fdb501c72807c8c17ae74fb36
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 15:06:31 -0300

    feat:criado o JwtFilter para receber os tokens jwt vindo do header.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/filters/JwtFilter.java
@@ -0,0 +1,60 @@
+package com.victorvilar.marketplace.fullstack.filters;
+
+import com.victorvilar.marketplace.fullstack.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final String AUTHORIZATION_HEADER = "Authorization";
+    private final String AUTHORIZATION_HEADER_PREFIX = "Bearer ";
+
+    @Autowired
+    public JwtFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = findToken(request);
+        if(token != null){
+           String subject = jwtService.verifyKey(token);
+           if(!subject.equals("")){
+               Authentication authentication = new UsernamePasswordAuthenticationToken(subject,null);
+               SecurityContextHolder.getContext().setAuthentication(authentication);
+           }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    /**
+     * Encontrao token se existir no header da requisição.
+     * O token normalmente vem com o prefixo 'Bearer ', que é removido caso o token exista
+     * @param request requisição HTTP
+     * @return string vazia caso não haja o header 'Authorization', ou o token
+     */
+    private String findToken(HttpServletRequest request){
+
+        String token = request.getHeader(AUTHORIZATION_HEADER);
+        if(token == null || !token.startsWith(AUTHORIZATION_HEADER_PREFIX)){
+            return null;
+        }
+        return token.replace(AUTHORIZATION_HEADER_PREFIX,"");
+    }
+}
+
+
+
