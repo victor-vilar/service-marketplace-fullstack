113639d2487ce457b536a79c4d981bb0fe80e08a
Victor Vilar <victor.svilar@gmail.com>
2026-05-29 13:39:01 -0300

    feat:criada uma configuração básica do spring security.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/configuration/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package com.victorvilar.marketplace.fullstack.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Profile("dev")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests(req ->{
+                    req.requestMatchers("/api/login").permitAll();
+                    req.anyRequest().authenticated();
+
+                })
+                //.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Profile("dev")
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        User admin = new User("admin",encoder.encode("pass"), List.of(new SimpleGrantedAuthority("user")));
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+}
