2b5b08d0759b7ff343231cdfa8549f6d3fda9edc
Victor Vilar <victor.svilar@gmail.com>
2026-05-31 15:35:34 -0300

    feat:criada classe category das ordens.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/domain/Category.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/domain/Category.java
@@ -2,16 +2,23 @@ package com.victorvilar.marketplace.fullstack.domain;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
 @Table(name="tb_categorias")
-public class Category {
+public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true)
     private String name;
+
+    public Category(){
+
+    }
 
     public UUID getId() {
         return id;
