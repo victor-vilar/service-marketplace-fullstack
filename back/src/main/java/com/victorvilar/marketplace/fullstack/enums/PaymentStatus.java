62671450a59d812b2d90f0e8cf3fbb89413a1151
Victor Vilar <victor.svilar@gmail.com>
2026-06-01 16:42:50 -0300

    fix: adicionado conteudo de paymentStatus.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/enums/PaymentStatus.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/enums/PaymentStatus.java
@@ -1,4 +1,19 @@
 package com.victorvilar.marketplace.fullstack.enums;
 
 public enum PaymentStatus {
+
+
+    ABERTA("Aberta"),
+    CONCLUIDO("Concluído");
+
+    private final String status;
+
+    PaymentStatus(String status){
+        this.status = status;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
 }
