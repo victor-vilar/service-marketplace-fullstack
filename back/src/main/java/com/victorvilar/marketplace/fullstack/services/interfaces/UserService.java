a7feaf8c0dcfc622c995ee178431df9f924e727d
Victor Vilar <victor.svilar@gmail.com>
2026-06-02 15:37:58 -0300

    refact:adicionado todos os metodos na interface.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/services/interfaces/UserService.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/services/interfaces/UserService.java
@@ -2,13 +2,19 @@ package com.victorvilar.marketplace.fullstack.services.interfaces;
 
 import com.victorvilar.marketplace.fullstack.domain.Job;
 import com.victorvilar.marketplace.fullstack.domain.User;
+import com.victorvilar.marketplace.fullstack.dtos.UserDTO;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface UserService extends CrudService<User>{
+public interface UserService{
 
-    public List<User> getAllActive();
-    public User getByIdWithJob(UUID id);
-    public User getByIdWithOrder(UUID id);
+    public List<UserDTO> getAll();
+    public UserDTO getById(UUID id);
+    public UserDTO save(UserDTO entity);
+    public UserDTO update(UserDTO entity);
+    public void delete(UUID id);
+    public List<UserDTO> getAllActive();
+    public UserDTO getByIdWithJob(UUID id);
+    public UserDTO getByIdWithOrder(UUID id);
 }
