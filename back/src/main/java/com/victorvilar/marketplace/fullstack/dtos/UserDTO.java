950b8abefecb77342fcf529a1aeb0f946918672d
Victor Vilar <victor.svilar@gmail.com>
2026-06-04 12:15:18 -0300

    feat:Criado builder para UserDTO.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/dtos/UserDTO.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/dtos/UserDTO.java
@@ -1,6 +1,64 @@
 package com.victorvilar.marketplace.fullstack.dtos;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-public record UserDTO(UUID id, String name, String email, String phoneNumber) {
+public record UserDTO(UUID id, String name, String email, String phoneNumber, List<JobDTO> jobs,List<OrderDTO> orders) {
+
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private UUID id = null;
+        private String name;
+        private String email;
+        private String phoneNumber = "";
+        private List<JobDTO> jobs = new ArrayList<>();
+        private List<OrderDTO> orders = new ArrayList<>();
+
+        public Builder id(UUID id){
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber){
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder jobs(List<JobDTO> jobs){
+            this.jobs = jobs;
+            return this;
+        }
+
+        public Builder orders(List<OrderDTO>orders){
+            this.orders = orders;
+            return this;
+        }
+
+        public UserDTO build(){
+
+            if(this.name == null || this.email == null){
+                throw new IllegalStateException("Um usuario deve possuir um nome, email e id");
+            }
+
+            return new UserDTO(id,name,email,phoneNumber,jobs,orders);
+
+        }
+
+
+    }
+
 }
