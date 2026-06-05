183cdfa71449802d28c4f208a559fa926e9ecad7
Victor Vilar <victor.svilar@gmail.com>
2026-06-05 14:32:41 -0300

    test:adicionado mais testes.

--- back/src/test/java/com/victorvilar/marketplace/fullstack/domain/UserTest.java
+++ back/src/test/java/com/victorvilar/marketplace/fullstack/domain/UserTest.java
@@ -1,6 +1,7 @@
 package com.victorvilar.marketplace.fullstack.domain;
 
 import com.victorvilar.marketplace.fullstack.enums.TipoUsuario;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,7 +9,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -37,5 +37,36 @@ class UserTest {
         List<? extends GrantedAuthority> authorities1 = (List<? extends GrantedAuthority>) user.getAuthorities();
         assertEquals(authorities1.get(0).toString(),TipoUsuario.ADMINISTRADOR.getTipo());
         assertEquals(authorities1.get(1).toString(),TipoUsuario.USUARIO.getTipo());
+    }
+
+    @Test
+    void deveAdicionarUmNovoAuthorityNaLista(){
+
+        user.addRole(TipoUsuario.ADMINISTRADOR);
+        Assertions.assertThat(user.getAuthorities().contains(TipoUsuario.ADMINISTRADOR.getTipo()));
+        Assertions.assertThat(!user.getAuthorities().contains(TipoUsuario.USUARIO.getTipo()));
+        org.junit.jupiter.api.Assertions.assertEquals(user.getAuthorities().size(), 1);
+        user.addRole(TipoUsuario.USUARIO);
+        Assertions.assertThat(user.getAuthorities().contains(TipoUsuario.USUARIO.getTipo()));
+        org.junit.jupiter.api.Assertions.assertEquals(user.getAuthorities().size(), 2);
+
+    }
+
+    @Test
+    void deveRemoverUmNovoAuthorityDaLista(){
+        user.addRole(TipoUsuario.ADMINISTRADOR);
+        user.addRole(TipoUsuario.USUARIO);
+        org.junit.jupiter.api.Assertions.assertEquals(user.getAuthorities().size(), 2);
+
+        user.removeRole(TipoUsuario.ADMINISTRADOR);
+        org.junit.jupiter.api.Assertions.assertEquals(user.getAuthorities().size(), 1);
+        Assertions.assertThat(!user.getAuthorities().contains(TipoUsuario.ADMINISTRADOR.getTipo()));
+        Assertions.assertThat(user.getAuthorities().contains(TipoUsuario.USUARIO.getTipo()));
+
+        user.removeRole(TipoUsuario.USUARIO);
+        org.junit.jupiter.api.Assertions.assertEquals(user.getAuthorities().size(), 0);
+        Assertions.assertThat(!user.getAuthorities().contains(TipoUsuario.USUARIO.getTipo()));
+
+
     }
 }
