e7dbba69326c2b268057534531562cf761f2a88c
Victor Vilar <victor.svilar@gmail.com>
2026-06-06 11:25:47 -0300

    feat:implementao metodo de login.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/LoginController.java
@@ -1,11 +1,17 @@
 package com.victorvilar.marketplace.fullstack.controllers;
 
+import com.victorvilar.marketplace.fullstack.domain.User;
+import com.victorvilar.marketplace.fullstack.dtos.LoginDTO;
 import com.victorvilar.marketplace.fullstack.services.JwtService;
 import com.victorvilar.marketplace.fullstack.services.implementation.UserServiceDefaultImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/login")
@@ -14,16 +20,29 @@ public class LoginController {
 
     private final JwtService jwtService;
     private final UserServiceDefaultImpl userServiceDefaultImpl;
+    private final AuthenticationManager authManager;
 
     @Autowired
-    public LoginController(JwtService jwtService, UserServiceDefaultImpl userServiceDefaultImpl){
+    public LoginController(JwtService jwtService, UserServiceDefaultImpl userServiceDefaultImpl, AuthenticationManager authManager){
         this.jwtService = jwtService;
         this.userServiceDefaultImpl = userServiceDefaultImpl;
+        this.authManager = authManager;
     }
 
-    @GetMapping
-    public String login(){
-        return "logado";
+    @PostMapping
+    public ResponseEntity<String> login(@Valid @RequestBody LoginDTO login){
+
+        //se não encontrar o spring lança erro automaticamente
+        var auth = new UsernamePasswordAuthenticationToken(login.username(),login.password());
+
+        //cria o token
+        var jwt = jwtService.generateKey((String) auth.getPrincipal());
+
+        //retorna token no body
+        return ResponseEntity.ok().body(jwt);
+
+
+
     }
 
 
