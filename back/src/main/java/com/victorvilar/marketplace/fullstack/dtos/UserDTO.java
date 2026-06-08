caf9199e3b3f45d2e1ce1ee70a334ee344cd5de4
Victor Vilar <victor.svilar@gmail.com>
2026-06-08 16:36:31 -0300

    refact:transformado userdto em classe ao inves de record.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/dtos/UserDTO.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/dtos/UserDTO.java
@@ -6,7 +6,88 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public record UserDTO(String id, @NotNull String name, @NotNull String email,String password, String phoneNumber, List<JobDTO> jobs, List<OrderDTO> orders) {
+public class UserDTO{
+
+    private String id = null;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber = "";
+    private List<JobDTO> jobs;
+    private List<OrderDTO> orders;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<JobDTO> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<JobDTO> jobs) {
+        this.jobs = jobs;
+    }
+
+    public List<OrderDTO> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<OrderDTO> orders) {
+        this.orders = orders;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserDTO(){
+
+    }
+
+    public UserDTO(String id, @NotNull String name, @NotNull String email, String password, String phoneNumber, List<JobDTO> jobs, List<OrderDTO> orders){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.jobs = jobs;
+        this.orders = orders;
+
+    }
+
 
     public static Builder builder(){
         return new Builder();
