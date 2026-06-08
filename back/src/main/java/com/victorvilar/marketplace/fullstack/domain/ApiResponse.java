3b48b37ba930ce4d938ae9097c38b2a6629130d9
Victor Vilar <victor.svilar@gmail.com>
2026-06-08 16:23:55 -0300

    feat: adicionado mais parametros ao api response.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/domain/ApiResponse.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/domain/ApiResponse.java
@@ -1,15 +1,22 @@
 package com.victorvilar.marketplace.fullstack.domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class ApiResponse<T> {
 
     private LocalDate date;
     private T response;
+    private boolean sucess;
+    private String message;
+    private List<String> errors;
 
-    public ApiResponse(LocalDate date, T object){
+    public ApiResponse(LocalDate date, T object,boolean sucess,String message, List<String> errors){
         this.date = date;
         this.response = object;
+        this.sucess = sucess;
+        this.message = message;
+        this.errors = errors;
     }
 
     public LocalDate getDate(){
