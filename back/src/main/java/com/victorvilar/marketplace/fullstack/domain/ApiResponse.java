f6ec35aa407f2f8fd1694e2621afd1d02b1413b6
Victor Vilar <victor.svilar@gmail.com>
2026-06-15 16:30:03 -0300

    feat:criado metodos estaticos para criação de respostas especificas.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/domain/ApiResponse.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/domain/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.victorvilar.marketplace.fullstack.domain;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApiResponse<T> {
@@ -10,6 +11,12 @@ public class ApiResponse<T> {
     private boolean sucess;
     private String message;
     private List<String> errors;
+    private static final String DEFAULT_SUCCESS = "Operação Realizada com Sucesso !";
+    private static final String DEFAULT_FAIL = "Erro !";
+
+    public ApiResponse(){
+
+    }
 
     public ApiResponse(T object,boolean sucess,String message, List<String> errors){
         this.response = object;
@@ -27,5 +34,47 @@ public class ApiResponse<T> {
     }
 
 
+    public static ResponseBuilder success(){
+        return new ResponseBuilder(true, DEFAULT_SUCCESS);
+    }
+
+    public static ResponseBuilder success(String message){
+        return new ResponseBuilder(true,message);
+    }
+
+    public static ResponseBuilder fail(List<String> errors){
+        return new ResponseBuilder(false,errors);
+    }
+
+    public static <T> ApiResponse<T> success(T response){
+        return success().build(response);
+    }
+
+    public static class ResponseBuilder{
+
+        private boolean success;
+        private String message = "";
+        private List<String> errors = new ArrayList<>();
+
+        public ResponseBuilder(boolean success, String message){
+            this.success = success;
+            this.message = message;
+        }
+
+        public ResponseBuilder(boolean success,List<String> errors){
+            this.success = success;
+            this.errors = errors;
+        }
+
+        public <T> ApiResponse<T> build(T response){
+            return new ApiResponse<>(response,this.success,this.message,this.errors);
+
+        }
+
+
+
+
+
+    }
 
 }
