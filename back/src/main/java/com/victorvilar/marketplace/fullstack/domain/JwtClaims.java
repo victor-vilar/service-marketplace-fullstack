bbbd44b34de8747412a7c1b9737e7d781051238f
Victor Vilar <victor.svilar@gmail.com>
2026-06-15 16:56:23 -0300

    feat: criado classe para armazenar o nomes dos claims do jwt.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/domain/JwtClaims.java
@@ -0,0 +1,9 @@
+package com.victorvilar.marketplace.fullstack.domain;
+
+public final class JwtClaims {
+
+    public static final String NAME = "name";
+    public static final String PHONE = "phone";
+    public static final String AUTHORITIES = "authorities";
+
+}
