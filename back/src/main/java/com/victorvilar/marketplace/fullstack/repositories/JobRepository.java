9e9e9467038684b478755d9fca977a1aa18b5c52
Victor Vilar <victor.svilar@gmail.com>
2026-06-17 15:32:29 -0300

    feat: criado jobRepository.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/repositories/JobRepository.java
@@ -0,0 +1,22 @@
+package com.victorvilar.marketplace.fullstack.repositories;
+
+import com.victorvilar.marketplace.fullstack.domain.Job;
+import com.victorvilar.marketplace.fullstack.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<Job, UUID> {
+
+    @Query("SELECT j FROM Job j LEFT JOIN FETCH j.orders WHERE j.id = :id")
+    public Optional<Job> findByIdWithOrders(@Param("id") UUID id);
+
+    public List<Job> findByProviderId(UUID providerId);
+    public List<Job> findByCategoryName(String category);
+
+
+}
