d92b5b55bbf588db7da5e6db091e70fb3a13daa1
Victor Vilar <victor.svilar@gmail.com>
2026-06-17 13:54:13 -0300

    feat: criado interface JobService.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/services/interfaces/JobService.java
@@ -0,0 +1,18 @@
+package com.victorvilar.marketplace.fullstack.services.interfaces;
+
+import com.victorvilar.marketplace.fullstack.domain.Job;
+import com.victorvilar.marketplace.fullstack.dtos.JobDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JobService {
+
+    public List<JobDTO> getAll();
+    public JobDTO getById(UUID id);
+    public List<JobDTO> getByProvider(UUID providerId);
+    public List<JobDTO> getByCategory(UUID categoryId);
+    public JobDTO update(JobDTO job);
+    public void delete(UUID id);
+    public JobDTO getByIdWithOrders(UUID id);
+}
