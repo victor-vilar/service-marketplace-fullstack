e8ea2dd1e5b20f843ae31a19cbc3a1493a780a4e
Victor Vilar <victor.svilar@gmail.com>
2026-06-21 11:14:15 -0300

    refact: adicionada propriedade id me jwtClaims.'

--- back/src/main/java/com/victorvilar/marketplace/fullstack/domain/JwtClaims.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/domain/JwtClaims.java
@@ -2,6 +2,7 @@ package com.victorvilar.marketplace.fullstack.domain;
 
 public final class JwtClaims {
 
+    public static final String ID ="id";
     public static final String NAME = "name";
     public static final String PHONE = "phone";
     public static final String AUTHORITIES = "authorities";
