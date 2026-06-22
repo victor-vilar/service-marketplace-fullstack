47671b9fd94f260294ebff92599a155460bdf5e0
Victor Vilar <victor.svilar@gmail.com>
2026-06-22 17:44:44 -0300

    fix/refact: registerController retorna uri do objeto criado, e nao cria mais o dto para passar para o service.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/RegisterController.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/RegisterController.java
@@ -1,6 +1,7 @@
 package com.victorvilar.marketplace.fullstack.controllers;
 
 import com.victorvilar.marketplace.fullstack.domain.ApiResponse;
+import com.victorvilar.marketplace.fullstack.domain.User;
 import com.victorvilar.marketplace.fullstack.dtos.RegisterDTO;
 import com.victorvilar.marketplace.fullstack.dtos.UserDTO;
 import com.victorvilar.marketplace.fullstack.services.interfaces.UserService;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDate;
 
 @RestController
@@ -21,26 +24,24 @@ public class RegisterController {
 
     private final UserService service;
     private static final String SUCCESS_MESSAGE =  "Registro realizado com sucesso !";
-    private final PasswordEncoder encoder;
 
     @Autowired
-    public RegisterController(UserService service, PasswordEncoder encoder){
+    public RegisterController(UserService service){
         this.service = service;
-        this.encoder = encoder;
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<UserDTO>> register(@Valid @RequestBody RegisterDTO register){
 
-        UserDTO user = UserDTO.builder()
-                .name(register.name())
-                .email(register.email())
-                .password(encoder.encode(register.password()))
-                .build();
+        UserDTO user = this.service.save(register);
 
-        user = this.service.save(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/api/users/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
         ApiResponse<UserDTO> response = ApiResponse.success(SUCCESS_MESSAGE).build(user);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(uri).body(response);
     }
 
 
