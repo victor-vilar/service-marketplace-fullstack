1ad0145e767eb6f1e1c37a7004e80d5153729468
Victor Vilar <victor.svilar@gmail.com>
2026-06-22 17:24:59 -0300

    fix: corrigido nome do endpoint.

--- back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/UserController.java
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/controllers/UserController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService service;
