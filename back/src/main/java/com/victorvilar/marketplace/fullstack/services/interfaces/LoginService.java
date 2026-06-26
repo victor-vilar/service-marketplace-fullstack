6388d13b6bf1bf0b5e9fce67af464b306ec84244
Victor Vilar <victor.svilar@gmail.com>
2026-06-26 17:27:02 -0300

    feat: criado login service.

---
+++ back/src/main/java/com/victorvilar/marketplace/fullstack/services/interfaces/LoginService.java
@@ -0,0 +1,8 @@
+package com.victorvilar.marketplace.fullstack.services.interfaces;
+
+import com.victorvilar.marketplace.fullstack.dtos.LoginDTO;
+
+public interface LoginService {
+
+    String login(LoginDTO login);
+}
